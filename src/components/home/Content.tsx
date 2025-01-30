04c669d4963de76c5a69594937c48eb6c447c1a5
brianwu <fslikeyou1033@yahoo.com>
2025-01-30 11:25:39 +0800

    add animation keyframe

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -1,41 +1,134 @@
 "use client";
 
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
+
+type keyframeProps = {
+    id: number;
+    opacityKeyframe: number[];
+    opacity: number[];
+    xKeyframe: number[];
+    x: number[];
+};
+
+type scrollProgressProps = {
+    scrollProgress: number;
+};
+
+const transformation: keyframeProps[] = [
+    {
+        id: 0,
+        opacityKeyframe: [0, 0.5],
+        opacity: [0, 1],
+        xKeyframe: [0, 0.5],
+        x: [100, 0],
+    },
+    {
+        id: 1,
+        opacityKeyframe: [0.1, 0.5],
+        opacity: [0, 1],
+        xKeyframe: [0, 0.6],
+        x: [100, 0],
+    },
+    {
+        id: 2,
+        opacityKeyframe: [0.4, 0.5],
+        opacity: [0, 1],
+        xKeyframe: [0.2, 0.7],
+        x: [-100, 0],
+    },
+];
+
 export default function Content({}) {
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "end center"],
+    });
+
     return (
-        <div className="relative w-full h-screen">
-            <AboutSection />
-            <WorkSection />
-            <ContactSection />
+        <div ref={ref} className="relative w-full h-screen">
+            <AboutSection
+                transformation={transformation[0]}
+                scrollProgress={scrollYProgress}
+            />
+            <WorkSection
+                transformation={transformation[1]}
+                scrollProgress={scrollYProgress}
+            />
+            <ContactSection
+                transformation={transformation[2]}
+                scrollProgress={scrollYProgress}
+            />
         </div>
     );
 }
 
-const AboutSection = ({}) => {
+const AboutSection = ({ transformation, scrollProgress }) => {
+    const opacity = useTransform(
+        scrollProgress,
+        transformation.opacityKeyframe,
+        transformation.opacity
+    );
+    const x = useTransform(
+        scrollProgress,
+        transformation.xKeyframe,
+        transformation.x
+    );
     return (
-        <section className="absolute top-1/4 left-1/2">
+        <motion.section
+            style={{ opacity: opacity, x: x }}
+            className="absolute top-1/4 left-1/2"
+        >
             <h1 className="text-xl">About</h1>
             <p className="text-md">I'm body paragraph box</p>
-        </section>
+        </motion.section>
     );
 };
 
-const WorkSection = ({}) => {
+const WorkSection = ({ transformation, scrollProgress }) => {
+    const opacity = useTransform(
+        scrollProgress,
+        transformation.opacityKeyframe,
+        transformation.opacity
+    );
+    const x = useTransform(
+        scrollProgress,
+        transformation.xKeyframe,
+        transformation.x
+    );
     return (
-        <section className="absolute right-16 top-8">
+        <motion.section
+            style={{ opacity: opacity, x: x }}
+            className="absolute right-16 top-8"
+        >
             <h2 className="text-lg">Work</h2>
             <p className="text-md">I'm body paragraph box 1</p>
             <p className="text-md">I'm body paragraph box 2</p>
-        </section>
+        </motion.section>
     );
 };
 
-const ContactSection = ({}) => {
+const ContactSection = ({ transformation, scrollProgress }) => {
+    const opacity = useTransform(
+        scrollProgress,
+        transformation.opacityKeyframe,
+        transformation.opacity
+    );
+    const x = useTransform(
+        scrollProgress,
+        transformation.xKeyframe,
+        transformation.x
+    );
     return (
-        <section className="absolute left-16 inset-y-1/2">
+        <motion.section
+            style={{ opacity: opacity, x: x }}
+            className="absolute left-16 inset-y-1/2"
+        >
             <h2 className="text-md">Contact</h2>
             <p className="text-sm">I'm body paragraph box 1</p>
             <p className="text-sm">I'm body paragraph box 2</p>
             <p className="text-sm">I'm body paragraph box 3</p>
-        </section>
+        </motion.section>
     );
 };
