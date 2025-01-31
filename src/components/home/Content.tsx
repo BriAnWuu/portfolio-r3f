bc4a6041ac20b1ffde6f59162d60d05946bfabf0
brianwu <fslikeyou1033@yahoo.com>
2025-01-31 14:40:13 +0800

    change scroll offset

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -3,6 +3,7 @@
 import clsx from "clsx";
 import { motion, useScroll, useSpring, useTransform } from "motion/react";
 import { useRef } from "react";
+import { AboutSection, ContactSection, WorkSection } from "./ContentSection";
 
 type keyframeProps = {
     id: number;
@@ -19,23 +20,23 @@ type scrollProgressProps = {
 const transformation: keyframeProps[] = [
     {
         id: 0,
-        opacityKeyframe: [0, 0.6],
+        opacityKeyframe: [0.1, 0.5],
         opacity: [0, 1],
-        xKeyframe: [0.1, 0.75],
+        xKeyframe: [0.1, 0.5],
         x: [200, 0],
     },
     {
         id: 1,
-        opacityKeyframe: [0.1, 0.5],
+        opacityKeyframe: [0, 0.5],
         opacity: [0, 1],
-        xKeyframe: [0, 0.75],
+        xKeyframe: [0, 0.5],
         x: [100, 0],
     },
     {
         id: 2,
-        opacityKeyframe: [0.4, 0.75],
+        opacityKeyframe: [0.2, 0.5],
         opacity: [0, 1],
-        xKeyframe: [0.2, 0.75],
+        xKeyframe: [0.2, 0.5],
         x: [-100, 0],
     },
 ];
@@ -44,11 +45,11 @@ export default function Content({}) {
     const ref = useRef(null);
     const { scrollYProgress } = useScroll({
         target: ref,
-        offset: ["start end", "end end"],
+        offset: ["start 0.8", "end end"],
     });
     const smoothScrollProgress = useSpring(scrollYProgress, {
         damping: 10,
-        stiffness: 100,
+        stiffness: 300,
     });
 
     return (
@@ -97,35 +98,5 @@ const Section = ({ children, transformation, scrollProgress, position }) => {
         >
             {children}
         </motion.section>
-    );
-};
-
-const AboutSection = ({}) => {
-    return (
-        <>
-            <h1 className="text-xl">About</h1>
-            <p className="text-md">I'm body paragraph box</p>
-        </>
-    );
-};
-
-const WorkSection = ({}) => {
-    return (
-        <>
-            <h2 className="text-lg">Work</h2>
-            <p className="text-md">I'm body paragraph box 1</p>
-            <p className="text-md">I'm body paragraph box 2</p>
-        </>
-    );
-};
-
-const ContactSection = ({}) => {
-    return (
-        <>
-            <h2 className="text-md">Contact</h2>
-            <p className="text-sm">I'm body paragraph box 1</p>
-            <p className="text-sm">I'm body paragraph box 2</p>
-            <p className="text-sm">I'm body paragraph box 3</p>
-        </>
     );
 };
---
+++ src/components/home/ContentSection.tsx
@@ -0,0 +1,29 @@
+export function AboutSection({}) {
+    return (
+        <>
+            <h1 className="text-xl">About</h1>
+            <p className="text-md">I'm body paragraph box</p>
+        </>
+    );
+}
+
+export function WorkSection({}) {
+    return (
+        <>
+            <h2 className="text-lg">Work</h2>
+            <p className="text-md">I'm body paragraph box 1</p>
+            <p className="text-md">I'm body paragraph box 2</p>
+        </>
+    );
+}
+
+export function ContactSection({}) {
+    return (
+        <>
+            <h2 className="text-md">Contact</h2>
+            <p className="text-sm">I'm body paragraph box 1</p>
+            <p className="text-sm">I'm body paragraph box 2</p>
+            <p className="text-sm">I'm body paragraph box 3</p>
+        </>
+    );
+}
