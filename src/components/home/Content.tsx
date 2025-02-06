4dff5c6b12e6630d5e1d1f91b440f6feac6bed0a
brianwu <fslikeyou1033@yahoo.com>
2025-02-06 14:31:36 +0800

    change content layout

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -11,49 +11,10 @@ import {
 import { ReactNode, useRef } from "react";
 import {
     AboutSection,
-    ContactSection,
+    ExperienceSection,
     TechSection,
     WorkSection,
 } from "./ContentSection";
-
-type keyframeProps = {
-    id: number;
-    opacityKeyframe: number[];
-    opacity: number[];
-    xKeyframe: number[];
-    x: number[];
-};
-
-const transformation: keyframeProps[] = [
-    {
-        id: 0,
-        opacityKeyframe: [0.3, 0.6],
-        opacity: [0, 1],
-        xKeyframe: [0.1, 0.5],
-        x: [50, 0],
-    },
-    {
-        id: 1,
-        opacityKeyframe: [0.3, 0.6],
-        opacity: [0, 1],
-        xKeyframe: [0, 0.5],
-        x: [50, 0],
-    },
-    {
-        id: 2,
-        opacityKeyframe: [0.3, 0.6],
-        opacity: [0, 1],
-        xKeyframe: [0.2, 0.5],
-        x: [-50, 0],
-    },
-    {
-        id: 3,
-        opacityKeyframe: [0.3, 0.6],
-        opacity: [0, 1],
-        xKeyframe: [0.2, 0.5],
-        x: [-50, 0],
-    },
-];
 
 export default function Content() {
     const ref = useRef(null);
@@ -68,32 +29,16 @@ export default function Content() {
 
     return (
         <div ref={ref} className="relative w-full bottom-1 h-[150vh]">
-            <SectionWrapper
-                transformation={transformation[0]}
-                scrollProgress={smoothScrollProgress}
-                position={"top-1/4 left-1/2"}
-            >
+            <SectionWrapper position={"top-[5%] left-[15%]"}>
                 <AboutSection />
             </SectionWrapper>
-            <SectionWrapper
-                transformation={transformation[1]}
-                scrollProgress={smoothScrollProgress}
-                position={"right-16 top-8"}
-            >
+            <SectionWrapper position={"top-[20%] left-[15%]"}>
                 <WorkSection />
             </SectionWrapper>
-            <SectionWrapper
-                transformation={transformation[2]}
-                scrollProgress={smoothScrollProgress}
-                position={"left-16 bottom-1/2"}
-            >
-                <ContactSection />
+            <SectionWrapper position={"top-[35%] left-[15%]"}>
+                <ExperienceSection />
             </SectionWrapper>
-            <SectionWrapper
-                transformation={transformation[3]}
-                scrollProgress={smoothScrollProgress}
-                position={"left-20 bottom-16"}
-            >
+            <SectionWrapper position={"top-[50%] left-[15%]"}>
                 <TechSection />
             </SectionWrapper>
         </div>
@@ -102,33 +47,43 @@ export default function Content() {
 
 const SectionWrapper = ({
     children,
-    transformation,
-    scrollProgress,
     position,
 }: {
     children: ReactNode;
-    transformation: keyframeProps;
-    scrollProgress: MotionValue<number>;
     position: string;
 }) => {
-    const opacity = useTransform(
-        scrollProgress,
-        transformation.opacityKeyframe,
-        transformation.opacity
-    );
-    const x = useTransform(
-        scrollProgress,
-        transformation.xKeyframe,
-        transformation.x
-    );
+    const variants = {
+        initial: {
+            opacity: 0,
+            y: 100,
+        },
+        inView: {
+            opacity: 1,
+            y: 0,
+        },
+    };
+
+    const transition = {
+        delay: 0.3,
+        duration: 0.4,
+        ease: "easeInOut",
+        type: "spring",
+        mass: 1,
+        damping: 10,
+        stiffness: 100,
+    };
 
     return (
         <motion.section
-            style={{
-                opacity: opacity,
-                x: x,
-            }}
             className={clsx("absolute", position)}
+            variants={variants}
+            initial="initial"
+            whileInView="inView"
+            transition={transition}
+            viewport={{
+                once: true,
+                margin: "0px 0px -7.5% 0px",
+            }}
         >
             {children}
         </motion.section>
--- src/components/home/ContentSection.tsx
+++ src/components/home/ContentSection.tsx
@@ -28,17 +28,6 @@ export function WorkSection() {
     );
 }
 
-export function ContactSection() {
-    return (
-        <>
-            <h2 className="text-md">Contact</h2>
-            <p className="text-sm">I'm body paragraph box 1</p>
-            <p className="text-sm">I'm body paragraph box 2</p>
-            <p className="text-sm">I'm body paragraph box 3</p>
-        </>
-    );
-}
-
 export function ExperienceSection() {
     return (
         <>
