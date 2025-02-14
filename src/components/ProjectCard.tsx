83ad75b0906f8bd712a2cb92154f9773ffd0f37d
brianwu <fslikeyou1033@yahoo.com>
2025-02-14 23:34:57 +0800

    add sqlite kysely

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import ProjectBgImage from "./ProjectBgImage";
 export default function ProjectCard({}) {
     return (
         // set width height
-        <article className="relative w-full h-[300px] flex flex-col bg-gray-700 opacity-90 rounded-lg overflow-hidden">
+        <article className="relative w-full h-[300px] flex flex-col bg-gray-700 opacity-95 rounded-lg overflow-hidden">
             <ProjectBgImage src="/images/Brian_Wu.jpg" alt="example image" />
             <section className="absolute bottom-4 w-auto h-[40%] flex flex-col bg-gray-700 px-4">
                 <h4 className="text-md mb-2">project title</h4>
