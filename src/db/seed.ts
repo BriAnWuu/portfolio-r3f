d32da5b06a7ee531742425a65bf984422ab1e184
brianwu <fslikeyou1033@yahoo.com>
2025-03-05 11:50:16 -0500

    add seed data

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -2,58 +2,72 @@ import { db } from "./index";
 import { ExperienceTalbe, ProjectTable } from "./schema";
 
 async function seed() {
+    await db.delete(ProjectTable);
+    await db.delete(ExperienceTalbe);
+
     const project = await db
         .insert(ProjectTable)
         .values([
             {
-                title:
+                id: 1,
+                title: "Magnum Opus",
                 description:
-                imageUrl:
-                projectUrl:
+                    "Magnum Opus is your favourite art auction platform for discovering great craftsmanship, creativity, and vision. Magnum Opus connects you with art that inspires and endures. Discover, bid, and own a piece of the extraordinary.",
+                imageUrl: "/images/project/magnum-opus.jpg",
+                projectUrl: "https://github.com/BriAnWuu/magnum-opus",
             },
             {
-                title:
+                id: 2,
+                title: "Portfolio Website",
                 description:
-                imageUrl:
-                projectUrl:
+                    "This very website. A portfolio website built with Next.js, Three.js, Motion, Tailwind CSS, Drizzle ORM, and SQLite",
+                imageUrl: "/images/project/portfolio-r3f.jpg",
+                projectUrl: "https://github.com/BriAnWuu/portfolio-r3f",
             },
             {
-                title:
+                id: 3,
+                title: "Timence",
                 description:
-                imageUrl:
-                projectUrl:
+                    "Timence is a calendar app built with React, Redux, Sass, and Material UI",
+                imageUrl: "/images/project/timence.jpg",
+                projectUrl: "https://github.com/BriAnWuu/timence",
             },
             {
-                title:
+                id: 4,
+                title: "Financial Risk Management",
                 description:
-                imageUrl:
+                    "A project from my quant finance degree, just to showcase a different aspect of my life as a software engineer. This project demonstrates the quantitative risks of financial assets, how to measure and mitigate them.",
+                imageUrl: "/images/project/financial-risk-management.jpg",
                 projectUrl:
+                    "https://github.com/BriAnWuu/Financial_Risk_Management",
             },
         ])
         .returning({
             projectId: ProjectTable.id,
             projectTitle: ProjectTable.title,
         })
-        .onConflictDoNothing({ target: ProjectTable.title });
+        .onConflictDoNothing({ target: ProjectTable.id });
 
     const experience = await db
         .insert(ExperienceTalbe)
         .values([
             {
-                role: "Research Assistant",
-                company: "RCEVI, Chang Gung University",
-                location: "Taoyuan, Taiwan",
-                industry: "Research",
-                startDate: new Date("2020-08-01"),
-                endDate: new Date("2021-05-31"),
-            },
-            {
+                id: 1,
                 role: "Software Engineer Intern",
                 company: "AGTOP Technology, Inc",
                 location: "Taipei, Taiwan",
                 industry: "Technology",
                 startDate: new Date("2018-07-01"),
                 endDate: new Date("2018-08-31"),
+            },
+            {
+                id: 2,
+                role: "Research Assistant",
+                company: "RCEVI, Chang Gung University",
+                location: "Taoyuan, Taiwan",
+                industry: "Research",
+                startDate: new Date("2020-08-01"),
+                endDate: new Date("2021-05-31"),
             },
         ])
         .returning({
---
+++ src/lib/get-items.ts
@@ -0,0 +1,45 @@
+import "server-only";
+
+import { db } from "@/db";
+import {
+    ExperienceTalbe,
+    ProjectTable,
+    SelectExperience,
+    SelectProject,
+} from "@/db/schema";
+import { asc, desc } from "drizzle-orm";
+
+export async function getProjects(
+    size = 4
+): Promise<Array<Partial<SelectProject>>> {
+    const projects = await db
+        .select({
+            id: ProjectTable.id,
+            title: ProjectTable.title,
+            description: ProjectTable.description,
+            imageUrl: ProjectTable.imageUrl,
+            projectUrl: ProjectTable.projectUrl,
+        })
+        .from(ProjectTable)
+        .orderBy(asc(ProjectTable.id))
+        .limit(size);
+    return projects;
+}
+
+export async function getExperiences(): Promise<
+    Array<Partial<SelectExperience>>
+> {
+    const experiences = await db
+        .select({
+            id: ExperienceTalbe.id,
+            role: ExperienceTalbe.role,
+            company: ExperienceTalbe.company,
+            location: ExperienceTalbe.location,
+            industry: ExperienceTalbe.industry,
+            startDate: ExperienceTalbe.startDate,
+            endDate: ExperienceTalbe.endDate,
+        })
+        .from(ExperienceTalbe)
+        .orderBy(desc(ExperienceTalbe.startDate));
+    return experiences;
+}
